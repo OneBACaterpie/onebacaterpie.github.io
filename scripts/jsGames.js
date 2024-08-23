208821a0e7811546a73bd10decae6c07e7345067
OneBACaterpie <voxgens@gmail.com>
2024-08-23 16:47:25 -0400

    Create jsGames.js

---
+++ scripts/jsGames.js
@@ -0,0 +1,15 @@
+const gameButtons = document.getElementById("gameSelectMenu").ElementsByClassName("gameSelectButton");
+console.log(gameButtons);
+let userChoice;
+const scriptsPath = "https://https://onebacaterpie.github.io/scripts/";
+
+function loadGame(userChoice) {
+  var script = document.createElement("script");
+  script.src = URL(userChoice.concat(".js"), scriptsPath);
+  document.head.appendChild(script);
+}
+
+gameButtons.forEach(gameButtons => gameButtons.addEventListener("click", (e) => {
+  userChoice = e.target.id;
+  loadGame(userChoice);
+}
