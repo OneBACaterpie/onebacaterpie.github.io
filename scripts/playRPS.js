04e88ec61c907d2cbe9438861e3c8e12d882d5bb
OneBACaterpie <voxgens@gmail.com>
2024-09-03 11:06:17 -0400

    Update playRPS.js

--- scripts/playRPS.js
+++ scripts/playRPS.js
@@ -5,24 +5,58 @@ const gameContainer = document.getElementById("gameContainer");
 // Clear Game Container
 gameContainer.replaceChildren();
 
-// Create elements
+// Create an empty array for RPS elements
+const rpsElements = [];
+
+// Create elements for Computer Choice and push to rpsElements array
 const computerHeader = document.createElement("h2");
 computerHeader.textContent = "Computer Choice: ";
 const computerDisplay = document.createElement("span");
+computerDisplay.setAttribute("id", "computerChoice");
+rpsElements.push(computerHeader, computerDisplay);
+
+// Create elements for User Choice and push to rpsElements array
 const userHeader = document.createElement("h2");
 userHeader.textContent = "Your Choice: ";
 const userDisplay = document.createElement("span");
-userHeader.appendChild(userDisplay);
+userDisplay.setAttribute("id", "userChoice");
+rpsElements.push(userHeader, userDisplay)
+
+// Create elements for Result and push to rpsElements array
 const resultHeader = document.createElement("h2");
+resultHeader.textContent = "Result: ";
+const displayResult = document.createElement("span");
+displayResult.setAttribute("id", "result");
+rpsElements.push(resultHeader, displayResult);
+
+// Rock button
 const rockButton = document.createElement("button");
+rockButton.textContent("Rock");
+rockButton.setAttribute("id", "rock");
+
+// Paper button
+const paperButton = document.createElement("button");
+rockButton.textContent("Paper");
+rockButton.setAttribute("id", "paper");
+
+// Scissors button
+const scissorsButton = document.createElement("button");
+scissorsButton.textContent("Scissors");
+scissorsButton.setAttribute("id", "scissors");
+
+// Push buttons to rpsElements array
+rpsElements.push(rockButton, paperButton, scissorsButton);
 
 // Add elements to Game Container
-gameContainer.appendChild("")
+for (let i = 0; i < rpsElements.length(); i++) {
+  gameContainer.appendChild(rpsElements[i]);
+}
 
 const computerChoiceDisplay = document.getElementById("computerChoice");
 const userChoiceDisplay = document.getElementById("userChoice");
 const resultDisplay = document.getElementById("result");
-const possibleChoices = document.querySelectorAll("button");
+const possibleChoices = [];
+possibleChoices.push(rockButton, paperButton, scissorsButton);
 let userChoice
 let computerChoice
 let result
