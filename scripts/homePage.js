6c3bb53d83b0be214c5b47fa214504589b6ef08c
OneBACaterpie <voxgens@gmail.com>
2025-05-19 14:54:36 -0400

    Create homePage.js

---
+++ scripts/homePage.js
@@ -0,0 +1,11 @@
+// Main script
+// Modules: player.js, state.js, input.js, utils.js
+window.addEventListener('load', function(){
+  const loading = document.getElementById('loading');
+  loading.style.display = 'none';
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  
+}
